b54deb709a5b42dd88611556e63962742187eec7
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-12-10 12:44:44 +0300

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
-
-import hogs from "../porkers_data";
+import HogList from "./HogList";
+import HogDetails from "./HogDetails";
+import hogsData from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  const [hogs, setHogs] = useState(hogsData);
+  const [selectedHog, setSelectedHog] = useState(null);
+  const [filterGreased, setFilterGreased] = useState(false);
+  const [sortOption, setSortOption] = useState("name"); // Default sort by name
+
+  const handleHogClick = (hog) => {
+    setSelectedHog(hog);
+  };
+
+  const handleHideClick = (hog) => {
+    setHogs((prevHogs) => prevHogs.filter((h) => h !== hog));
+  };
+
+  return (
+    <div className="App">
+      <Nav
+        onToggleGreased={() => setFilterGreased((prevFilterGreased) => !prevFilterGreased)}
+        onSortChange={(option) => setSortOption(option)}
+        filterGreased={filterGreased}
+      />
+      <HogList
+        hogs={hogs}
+        onHogClick={handleHogClick}
+        onHideClick={handleHideClick}
+        filterGreased={filterGreased}
+        sortOption={sortOption}
+      />
+      {selectedHog && <HogDetails hog={selectedHog} onClose={() => setSelectedHog(null)} />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,19 @@
 import React from "react";
-import piggy from "../assets/porco.png";
 
-const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">HogWild</span>
-			<div className="TwirlyPig">
-				<img src={piggy} className="App-logo" alt="piggy" />
-			</div>
-			<span className="normalText">
-				A React App for County Fair Hog Fans
-			</span>
-		</div>
-	);
+const Nav = ({ onToggleGreased, onSortChange, filterGreased }) => {
+  return (
+    <div className="navWrapper">
+      <button className="ui button" onClick={onToggleGreased}>
+        {filterGreased ? "Show All Hogs" : "Show Greased Hogs"}
+      </button>
+      <div className="ui selection dropdown">
+        <select className="ui dropdown" onChange={(e) => onSortChange(e.target.value)}>
+          <option value="name">Sort by Name</option>
+          <option value="weight">Sort by Weight</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
